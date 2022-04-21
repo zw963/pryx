a799e83a4323765d9b6e92864424f6bdb2d2d892
Billy.Zheng <vil963@gmail.com>
2022-04-21 20:47:13 +0800

    Fix version to work with ritual

--- lib/pryx/version.rb
+++ lib/pryx/version.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 module Pryx
-  VERSION = '0.2.1'
+  VERSION = [0, 2, 1]
+
+  class << VERSION
+    include Comparable
+
+    def to_s
+      join('.')
+    end
+  end
 end
