ec7544589a2e959c853cee818c6a669b0143c7d7
Billy.Zheng <vil963@gmail.com>
2022-05-05 23:02:41 +0800

    move ap config into common_plugins.rb

--- lib/pryx/common_plugins.rb
+++ lib/pryx/common_plugins.rb
@@ -1,11 +1,23 @@
 require 'binding_of_caller'
 require 'clipboard'
 
-# ap_hack 可以确保，当使用 irb! 时，looksee ls1 输出显示正确，同时不用关闭 irb :USE_COLORIZE 设置。
+# HACK: for ap 可以确保，当使用 irb! 时，looksee ls1 输出显示正确，同时不用关闭 irb :USE_COLORIZE 设置。
 # See https://github.com/oggy/looksee/issues/57
 # 但是，ap_hack 必须放到 break_hack 前面，调换顺序，上面的 hack 失效。
-# WARN: 下面两行代码顺序不要换。
-require 'pryx/ap_hack'
+# WARN: 下面两行 require 代码顺序不要换。
+require 'awesome_print'
+
+# AwesomePrint.defaults = {
+#   index: false
+# }
+
+if defined? AwesomePrint
+  if defined? Pry
+    AwesomePrint.pry!
+  else
+    AwesomePrint.irb!
+  end
+end
 
 # Add break gem into pryx for fix pry-remote issue.
 # See https://github.com/gsamokovarov/break/issues/9
