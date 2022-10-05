591d16898eeefaecf1bdc8b93cba7efb65fc55be
Billy.Zheng <vil963@gmail.com>
2022-10-05 23:51:41 +0800

    Refactor pryx, irbx

--- lib/pryx_cli.rb
+++ lib/pryx_cli.rb
@@ -1,12 +1,7 @@
-require 'clipboard'
-
-require 'pryx/ap_hack'
-
 # This require is needed, because can't require looksee in method when use with bundler.
 # bundler remove looksee from $LOAD_PATH when start with rails.
 require 'looksee'
-require 'pryx/looksee_hack'
-
+require 'pryx/common_plugins'
 require 'pryx'
 
 if File.exist? 'config/environment.rb'
